42bd975199b3c53742e52589a30d9eef040ab4f9
Moisés Rocha <moisesrocha99@gmail.com>
2019-09-27 12:05:43 +0100

    sphere memory fix

--- MySceneGraph.js
+++ MySceneGraph.js
@@ -842,6 +842,6 @@ class MySceneGraph {
         //To do: Create display loop for transversing the scene graph
 
         //To test the parsing/creation of the primitives, call the display function directly
-        this.primitives['demoTriangle'].display();
+        this.primitives['demoSphere'].display();
     }
 }
--- MySphere.js
+++ MySphere.js
@@ -13,19 +13,14 @@ class MySphere extends CGFobject {
         this.vertices = [];
         this.indices = [];
         this.normals = [];
-        this.texCoords = [];
-
-        console.log(Math.PI);
-        console.log(this.slices);
-        console.log(this.stacks);
 
         let dPhi = 2 * Math.PI / this.slices;
         let dTheta = Math.PI / (this.stacks * 2 - 1);
 
+        //One Pole
         this.vertices.push(...[0,0,1]);
         this.normals.push(...[0,0,1]);
 
-        let lastIndex = 1;
         for (let i = 1; i <= this.stacks * 2 - 1; i++) {
             for (let j = 0; j < this.slices; j++) {
                 let x = Math.sin(dTheta*i) * Math.cos(dPhi*j);
@@ -34,43 +29,42 @@ class MySphere extends CGFobject {
 
                 this.normals.push(...[x,y,z]);
                 this.vertices.push(...[this.radius*x, this.radius*y, this.radius*z]);
-
-                lastIndex++;
             }
         }
 
+        //Other pole
         this.vertices.push(...[0,0,-1]);
         this.normals.push(...[0,0,-1]);
-        lastIndex++;
 
+        //Triangles on first pole
         for (let i = 0; i < this.slices; i++) {
             this.indices.push(i + 1);
             this.indices.push(i + 2);
             this.indices.push(0);
         }
 
-        let firstIndex = 1 + this.slices;
+        let firstIndex = 1;
         for(let i = 0; i < this.stacks * 2 - 2; i++) {
-            for(let j = 0; i < this.slices; j++) {
-                let base = firstIndex + j; 
+            for(let j = 0; j < this.slices; j++) {
                 //First triangle
-                this.indices.push(base);
-                this.indices.push(base - this.slices + 1);
-                this.indices.push(base - this.slices);
+                this.indices.push(firstIndex + j);
+                this.indices.push(firstIndex + j + this.slices);
+                this.indices.push(firstIndex + this.slices + ((j+1) % this.slices));
 
                 //Second triangle
-                this.indices.push(base);
-                this.indices.push(base + 1);
-                this.indices.push(base - this.slices + 1);
+                this.indices.push(firstIndex + j);
+                this.indices.push(firstIndex + this.slices + ((j+1) % this.slices));
+                this.indices.push(firstIndex + ((j + 1) % this.slices));
             }
 
             firstIndex += this.slices;
         }
 
+        //Triangles on second pole 
         for (let i = 0; i < this.slices; i++) {
             this.indices.push(firstIndex + i);
-            this.indices.push(firstIndex + i + 1);
-            this.indices.push(lastIndex);
+            this.indices.push(firstIndex + (i + 1) % this.slices);
+            this.indices.push(this.vertices.length / 3 - 1);
         }
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
