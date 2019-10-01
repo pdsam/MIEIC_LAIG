37f7222f673ab2e3e8ab6b52e59ba82728583015
Moisés Rocha <moisesrocha99@gmail.com>
2019-10-01 19:37:09 +0100

    added implementation of triangle texture coordinates

--- MyTriangle.js
+++ MyTriangle.js
@@ -32,6 +32,18 @@ class MyTriangle extends CGFobject{
             0,0,1
         ];
 
+        let a = Math.sqrt(Math.pow(this.x1-this.x3,2) + Math.pow(this.y1-this.y3,2) + Math.pow(this.z1-this.z3,2));
+        let b = Math.sqrt(Math.pow(this.x2-this.x1,2) + Math.pow(this.y2-this.y1,2) + Math.pow(this.z2-this.z1,2));
+        let c = Math.sqrt(Math.pow(this.x3-this.x2,2) + Math.pow(this.y3-this.y2,2) + Math.pow(this.z3-this.z2,2));
+
+        let beta = Math.acos(a*a - b*b + c*c) / (2 * a * c);
+
+        let v = a*Math.sin(beta);
+        this.texCoords.push(...[
+            c - a * Math.cos(beta), v - a*sin(beta),
+            0, v,
+            c, v
+        ]);
 
         this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
