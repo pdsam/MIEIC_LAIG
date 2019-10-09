a97e8f44d7c1fc20d6f2ec2bf2e15a8027f632bb
Moisés Rocha <moisesrocha99@gmail.com>
2019-10-09 16:27:50 +0100

    sphere uv coordinates finally working

--- MySphere.js
+++ MySphere.js
@@ -18,61 +18,48 @@ class MySphere extends CGFobject {
         let dPhi = 2 * Math.PI / this.slices;
         let dTheta = Math.PI / (this.stacks * 2 - 1);
 
-        //One Pole
-        this.vertices.push(...[0,0,1]);
-        this.normals.push(...[0,0,1]);
-        this.texCoords.push(...[0.5 + Math.atan2(0,0) / (2*Math.PI), 0]);
+        let circVertNum = this.slices + 1;
 
-        let slices = this.slices + 1;
+        //One Pole
+        for (let  i = 0; i < circVertNum; i++) {
+            this.vertices.push(...[0,0,1]);
+            this.normals.push(...[0,0,1]);
+            this.texCoords.push(...[i/this.slices, 1]);
+        }
 
         for (let i = 1; i <= (this.stacks * 2 - 1); i++) {
-            for (let j = 0; j < slices; j++) {
+            let z = Math.cos(dTheta*i);
+            for (let j = 0; j < circVertNum; j++) {
                 let x = Math.sin(dTheta*i) * Math.cos(dPhi*j);
                 let y = Math.sin(dTheta*i) * Math.sin(dPhi*j);
-                let z = Math.cos(dTheta*i);
 
                 this.normals.push(...[x,y,z]);
                 this.vertices.push(...[this.radius*x, this.radius*y, this.radius*z]);
-                let texC = [0.5 + Math.atan2(x,y) / (2*Math.PI), z * 0.5 + 0.5];
-                console.log(texC);
+                let texC = [1 - j/this.slices, z * 0.5 + 0.5];
                 this.texCoords.push(...texC);
             }
         }
 
         //Other pole
-        this.vertices.push(...[0,0,-1]);
-        this.normals.push(...[0,0,-1]);
-        this.texCoords.push(...[0.5 + Math.atan2(0,0) / (2*Math.PI), 1]);
-
-        //Triangles on first pole
-        for (let i = 0; i < slices; i++) {
-            this.indices.push(1 + i);
-            this.indices.push(1 + (i + 1) % slices);
-            this.indices.push(0);
+        for (let  i = 0; i < circVertNum; i++) {
+            this.vertices.push(...[0,0,-1]);
+            this.normals.push(...[0,0,-1]);
+            this.texCoords.push(...[i/this.sclices, 0]);
         }
 
-        let firstIndex = 1;
-        for(let i = 0; i < this.stacks * 2 - 2; i++) {
-            for(let j = 0; j < slices; j++) {
+        for(let i = 0; i < this.stacks * 2; i++) {
+            for(let j = 0; j < this.slices; j++) {
+                let current = circVertNum*i + j;
                 //First triangle
-                this.indices.push(firstIndex + j);
-                this.indices.push(firstIndex + j + slices);
-                this.indices.push(firstIndex + slices + ((j+1) % slices));
+                this.indices.push(current);
+                this.indices.push(current + circVertNum);
+                this.indices.push(current + circVertNum + 1);
 
                 //Second triangle
-                this.indices.push(firstIndex + j);
-                this.indices.push(firstIndex + slices + ((j+1) % slices));
-                this.indices.push(firstIndex + ((j + 1) % slices));
+                this.indices.push(current);
+                this.indices.push(current + circVertNum + 1);
+                this.indices.push(current + 1);
             }
-
-            firstIndex += slices;
-        }
-
-        //Triangles on second pole 
-        for (let i = 1; i < slices; i++) {
-            this.indices.push(this.vertices.length / 3 - 1);
-            this.indices.push(firstIndex + (i + 1) % slices);
-            this.indices.push(firstIndex + i);
         }
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
