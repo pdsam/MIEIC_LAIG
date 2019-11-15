0f16babeeb49c5225f0a5051f71d2786b0989316
Paulo Marques <paulodsam@gmail.com>
2019-11-15 11:34:34 +0000

    Parsing patch

--- Patch.js
+++ Patch.js
@@ -20,17 +20,18 @@ class Patch extends CGFobject{
         for(let i = 0; i <this.nPointsU;i++ ){
             let subBuffer = [];
             for(let j = 0; j < this.npointsV;j++){
-                subBuffer.push(this.controlPoints[i*this.npointsV+j]);
-                point.push(1);
-
+               let point =  Object.values(this.controlPoints[i*this.npointsV+j]);
+               point.push(1);
+                subBuffer.push(point);
             }
             controlPoints.push(subBuffer);
         }
 
-
+        let surface = new CGFnurbsSurface(this.nPointsU-1,this.npointsV-1,controlPoints);
+        this.nurbsPatch = new CGFobject(this.scene,this.npartsU,this.npartsV,surface);
     }
 
     display(){
-        console.log("displayed");
+        this.nurbsPatch.display();
     }
 }
