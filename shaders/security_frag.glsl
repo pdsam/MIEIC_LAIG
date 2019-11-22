37f2f73cad56dd1c64d94fb27b1892cd598f3e00
Moisés Rocha <moisesrocha99@gmail.com>
2019-11-22 09:52:32 +0000

    camera shader correction

--- shaders/security_frag.glsl
+++ shaders/security_frag.glsl
@@ -1,7 +1,6 @@
 #version 300 es
 precision highp float;
 
-in vec3 vPosition;
 in vec2 vTextureCoord;
 
 uniform sampler2D uSampler;
@@ -20,7 +19,7 @@ void main() {
 
     vec4 color = vec4(texColor.xyz * perc, 1);
 
-    float offset = sin((vTextureCoord.y + time) * 20.0)*0.5 + 1.0;
+    float offset = sin((vTextureCoord.y + time) * 20.0)*0.5 + 0.5;
 
-    fragColor = color + offset * 0.3;
+    fragColor = color + offset * 0.5;
 }
